cd77cce18d3bb974073a5530032dcb75b398b7a6
KlooNSizmicO <wilmardaniel@outlook.com>
2020-06-13 22:45:53 -0500

    componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {QueEsIntegracionComponent} from './components/que-es-integracion/que-es-integracion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QueEsIntegracionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/que-es-integracion/que-es-integracion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-que-es-integracion',
+  templateUrl: './que-es-integracion.component.html',
+  styleUrls: ['./que-es-integracion.component.css']
+})
+export class QueEsIntegracionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
